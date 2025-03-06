986bd825839fd1964387d1d03ae5ff60e9455711
Johannes Pflaum <jpflaum@gmx.de>
2025-03-06 14:18:45 +0100

    added routes for all teams on home site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,10 @@
 	import { base } from '$app/paths';
 </script>
 
-<p>Scouting 2025</p>
-<a href="{base}/dragons">dragons</a>
+<div class="grid gap-2 p-2">
+	<p>Scouting 2025</p>
+	<a href="{base}/dragons">Dragons</a>
+	<a href="{base}/jokers">Jokers</a>
+	<a href="{base}/sharks">Sharks</a>
+	<a href="{base}/spiders">Spiders</a>
+</div>
